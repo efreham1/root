5992690cc8f8042fe47119c3eaa1b7d8ea07a570
viwa3399 <viktor.wallsten.3399@student.uu.se>
2023-01-02 15:26:57 +0100

    documentation

--- src/stack_scanner.h
+++ src/stack_scanner.h
@@ -7,6 +7,10 @@
 #ifndef __stack__
 #define __stack__
 
+/// @brief scanns the stack for addresses that might be pointers to the heap
+/// @param top_valid_address the stack top adress
+/// @param bottom_valid_address the stack bottom adress
+/// @param len the number of adresses gets saved to this adress
 void ***stack_addresses(void *top_valid_address, void *bottom_valid_address, int *len);
 
 #endif
