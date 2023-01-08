845625082f379d9c6451d60325a12615cf182e0f
viwa3399 <90697986+viwa3399@users.noreply.github.com>
2023-01-08 19:42:04 +0100

    Update mover.h

--- src/mover.h
+++ src/mover.h
@@ -7,9 +7,21 @@
 #ifndef __mover__
 #define __mover__
 
+/// @brief the acctual garbage collection
+/// @param i_heap the internal heap
+/// @param stack_ptrs pointers from the stack
+/// @param ptrs_len number of pointers from the stack
+/// @param unsafe_stack if the stack is to be considerd safe or not
+/// @param proof_reading_arr checks if a pointer is equal to its proof reading pointer
 void move (internal_heap_t *i_heap, void ***stack_ptrs, int ptrs_len, bool unsafe_stack, void **proof_reading_arr);
 
-void
-do_move (void **data_ptr, page_t **new_pages, int len, page_t **static_pages, int static_len, internal_heap_t *i_heap);
+/// @brief moves data from a page to a new page
+/// @param data_ptr used to get the metadata
+/// @param new_pages pages to move to
+/// @param len number of new pages
+/// @param static_pages static pages
+/// @param static_len number of static pages
+/// @param i_heap the internal heap
+void do_move (void **data_ptr, page_t **new_pages, int len, page_t **static_pages, int static_len, internal_heap_t *i_heap);
 
 #endif
